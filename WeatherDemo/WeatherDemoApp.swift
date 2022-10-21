a671b510d5cfc5f675ec619ca1f8831ed7d3db9a
Steve Hobbs <steve.hobbs@auth0.com>
2022-10-21 21:15:18 +0100

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Steve Hobbs on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
